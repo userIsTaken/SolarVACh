694d6c78e2268f7ccb4b6b854204fbbf6a9de52a
userIsTaken <user@user.com>
2019-01-07 09:58:49 +0200

    fb scan

--- Solar_GUI.py
+++ Solar_GUI.py
@@ -370,7 +370,7 @@ class ApplicationWindow(QtWidgets.QMainWindow):
         # print('current_mean', data_mean)
         # print('err_rate', err_rate)
         # print('totalV', totalV)
-        self.ExperimentInfo('Current '+ str(round(data_mean, 4))+"\n"+'U : '+str(round(totalV, 4)))
+        self.ExperimentInfo('Current '+ str(round(data_mean, 7))+"\n"+'U : '+str(round(totalV, 4)))
         self.ui.live_error.setText(str(round(err_rate, 5)))
         array = np.arange(0, self.parameters['array_size'], 1)
         self.draw_method(self.ui.current_graph, '6th dimension', 'a.u.', 'Current', 'A', array, curr_array, False)
