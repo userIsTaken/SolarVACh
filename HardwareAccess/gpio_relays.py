83cf999866561205f230c1f105cfac5f136a374f
romas <romualdascepas@gmail.com>
2019-02-04 14:38:54 +0200

    GPIO relay class sketch

--- HardwareAccess/gpio_relays.py
+++ HardwareAccess/gpio_relays.py
@@ -1,4 +1,33 @@
-from gpiozero import *
+import gpiozero
 from gpiozero.pins.pigpio import PiGPIOFactory
 
 
+
+class RelayToggle():
+    def __init__(self, _ip_address:str = '192.168.0.104'):
+        """
+
+        :param _ip_address: TCP/IP address of source meter
+        """
+        self.RELAY_1 = 5
+        self.RELAY_2 = 6
+        self.RELAY_3 = 13
+        self.RELAY_4 = 19
+        self.RELAY_5 = 26
+        self.RELAY_6 = 12
+        self.factory = PiGPIOFactory(_ip_address)
+        self.ON = True
+        self.OFF = False
+        self.relay = None
+        pass
+
+    def set_relay(self, RELAY_PIN):
+        self.relay = gpiozero.OutputDevice(RELAY_PIN, active_high=False, initial_value=False)
+        pass
+
+    def toggle_relay(self, status):
+        if status:
+            self.relay.on()
+        else:
+            self.relay.off()
+        pass
