5817ba03d80dadea0bda4803c168584dc413f50e
userIsTaken <user@user.com>
2019-02-21 15:51:59 +0200

    small improvements :)

--- HardwareAccess/gpio_relays.py
+++ HardwareAccess/gpio_relays.py
@@ -1,8 +1,10 @@
 import os
-os.environ['GPIOZERO_PIN_FACTORY']="pigpio"
-os.environ['PIGPIO_ADDR']="192.168.0.104"
-import gpiozero
-from gpiozero.pins.pigpio import PiGPIOFactory
+import platform
+if 'arm' in platform.machine():
+    os.environ['GPIOZERO_PIN_FACTORY']="pigpio"
+    os.environ['PIGPIO_ADDR']="192.168.0.104"
+    import gpiozero
+    from gpiozero.pins.pigpio import PiGPIOFactory
 
 class RelayToggle():
     def __init__(self, RELAY:str, _ip_address:str = '192.168.0.104'):
